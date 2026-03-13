dea6c0b086bd3fc961134f5fba6e59d00f6791f0
Vewronika <veronika2801k@gmail.com>
2026-03-13 13:35:37 +0100

    .

---
+++ SE1_work1/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//Console.WriteLine("Hello, World!");
+using System.Text;
+
+namespace SE1_work1
+{
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         
+        }
+    }
+
+
+
+
+    public class StringCalculator
+    {
+        public int Calculate(string arg)
+        {
+            if (string.IsNullOrEmpty(arg))
+            {
+                return 0;
+            }
+
+            if (!arg.Contains("//"))
+            {
+
+
+
+                if (int.TryParse(arg, out int number))
+                {
+                    return number;
+                }
+
+                var nums = arg.Replace('\n', ',');
+
+
+
+
+                var numbers = nums.Split(',').Select(int.Parse);
+
+                if (numbers.Count() == 2 || numbers.Count() == 3)
+                {
+                    return numbers.Sum();
+                }
+
+            }
+            else
+            {
+
+                StringBuilder str = new StringBuilder();
+
+                foreach (char c in arg)
+                {
+                    if (IsNum(c)) continue;
+
+                    str.Append(c);
+
+
+
+                    if (IsNum(arg[arg.IndexOf(c) + 1]))
+                    {
+
+                    }
+                }
+
+
+
+
+            }
+
+            return -1;
+        }
+
+
+        public bool IsNum(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+    }
+
+
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,122 @@
+﻿using SE1_work1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class StringCalculatorTests
+    {
+        [TestMethod]
+        public void EmptyStringReturnsZero()
+        {
+            StringCalculator sc = new StringCalculator();
+
+            int result = sc.Calculate("");
+
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void SingleNumberReturnsValue()
+        {
+            StringCalculator sc = new StringCalculator();
+
+            int r1 = sc.Calculate("5");
+            Assert.AreEqual(5, r1);
+
+            int r2 = sc.Calculate("367");
+            Assert.AreEqual(367, r2);
+
+        }
+
+
+        [TestMethod]
+        public void DoubleNumberCommaDelimitedValue()
+        {
+            StringCalculator sc = new StringCalculator();
+
+            int result = sc.Calculate("5,6");
+
+            Assert.AreEqual(11, result);
+        }
+
+
+        [TestMethod]
+        public void DoubleNumberNewlineDelimitedValue()
+        {
+            StringCalculator sc = new StringCalculator();
+
+            int result = sc.Calculate("5\n9");
+
+            Assert.AreEqual(14, result);
+        }
+
+
+
+        [TestMethod]
+        public void ThreeNumbersAnyDelimitedValue()
+        {
+            StringCalculator sc = new StringCalculator();
+
+            int res1 = sc.Calculate("5,9,7");
+
+            Assert.AreEqual(21, res1);
+
+            int res2 = sc.Calculate("5\n10\n7");
+
+            Assert.AreEqual(22, res2);
+        }
+
+
+
+
+        [TestMethod]
+        public void NegativeNumbersThrowException()
+        {
+            StringCalculator sc = new StringCalculator();
+            Assert.ThrowsException<ArgumentException>(() => sc.Calculate("-5,6"));
+        }
+
+
+
+
+        [TestMethod]
+        public void NumbersGreaterThan1000AreIgnored()
+        {
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Calculate("5,1001");
+            Assert.AreEqual(5, result);
+        }
+
+
+
+
+        [TestMethod]
+        public void SingleCharDelimiterDefinedOnFirstLine()
+        {
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Calculate("//#\n5#6");
+            Assert.AreEqual(11, result);
+        }
+
+
+
+        [TestMethod]
+        public void MultiCharDelimiterDefinedOnFirstLine()
+        {
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Calculate("//[###]\n###6");
+            Assert.AreEqual(11, result);
+        }
+
+
+
+        [TestMethod]
+        public void MultipleDelimitersDefinedOnFirstLine()
+        {
+            StringCalculator sc = new StringCalculator();
+            int result = sc.Calculate("//[*][%]\n5*6%7");
+            Assert.AreEqual(18, result);
+        }
+    }
+}
